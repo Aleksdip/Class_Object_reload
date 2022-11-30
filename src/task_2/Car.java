cc22a944fac180fc4dcea7e9fa457f8b46a28387
Aleksdip <aleks.viunoff@gmail.com>
2022-11-30 18:05:09 +0200

    Змінив значеня параметрів у конструкторах

--- src/task_2/Car.java
+++ src/task_2/Car.java
@@ -15,23 +15,23 @@ public class Car {
 
     public Car(int age) {
         this.age = age;
-        this.speed = 200.0;
-        this.weight = 1500;
-        this.color = "blue";
+        this.speed = 202.8;
+        this.weight = 1590;
+        this.color = "white";
     }
 
     public Car(int age, double speed) {
         this.age = age;
         this.speed = speed;
-        this.weight = 1500;
-        this.color = "blue";
+        this.weight = 1980;
+        this.color = "black";
     }
 
     public Car(int age, double speed, int weight) {
         this.age = age;
         this.speed = speed;
         this.weight = weight;
-        this.color = "blue";
+        this.color = "yellow";
     }
 
     public Car(int age, double speed, int weight, String color) {
